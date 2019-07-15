bafb1fd435679bd3b07b15120d346ac656c1779c
Elie Puffelis <eliepuffelis@gmail.com>
2019-07-15 16:59:43 -0700

    Added bootstrap configurations

---
+++ themes/custom/elie/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.elie = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
